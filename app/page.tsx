7e8e61d00370495beedf90e6eaca06625a60ade5
Sid <oxyprogrammer@gmail.com>
2024-11-26 13:33:24 +0530

    Cleaning.

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,12 @@ export default function Home() {
 
   const envColorChoice = process.env.NEXT_PUBLIC_DEPLOYMENT_COLOR as ColorChoice;
 
-  console.log(`Color Choice from environment variable is ${process.env.NEXT_PUBLIC_DEPLOYMENT_COLOR || 'NOT DEFINED OR FALSY'}`)
-  
+  console.log(
+    `Color Choice from environment variable is ${
+      process.env.NEXT_PUBLIC_DEPLOYMENT_COLOR || 'NOT DEFINED OR FALSY'
+    }`
+  );
+
   const headingColor: string =
     envColorChoice && colors[envColorChoice] ? colors[envColorChoice] : colors.black;
 
@@ -25,7 +29,9 @@ export default function Home() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
-        <h1 className='text-3xl' style={{ color: headingColor }}>{message}</h1>
+        <h1 className='text-3xl' style={{ color: headingColor }}>
+          {message}
+        </h1>
       </main>
     </div>
   );
